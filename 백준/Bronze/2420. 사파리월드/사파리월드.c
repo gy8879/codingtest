4779b0a4e13865f932e9450c439d925192c54c56
gy8879 <99649795+gy8879@users.noreply.github.com>
2026-04-10 22:31:58 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    long long n,m;
+    scanf("%lld %lld", &n,&m);
+    
+    if(n>=m){
+        printf("%lld", n-m);
+    }else{
+        printf("%lld", m-n);
+    }
+    return 0;
+}
