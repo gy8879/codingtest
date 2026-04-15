c76c22423f426905e0c45e89d80add7d78227267
gy8879 <99649795+gy8879@users.noreply.github.com>
2026-04-16 00:38:42 +0900

    Title: 특정 위치의 문자, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260416/특정 위치의 문자/char-in-specific-location.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char word[6] = {'L','E','B','R','O','S'};
+    char a;
+    scanf("%c", &a);
+    int idx=-1;
+
+    for(int i=0; i<6; i++){
+        if(word[i]==a){
+            printf("%d",i);
+            idx=0;
+            break;
+        }
+    }
+    if(idx==-1){
+        printf("None");
+    }
+    // Please write your code here.
+    return 0;
+}
